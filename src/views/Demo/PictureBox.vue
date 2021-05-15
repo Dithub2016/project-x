5c9ebc45e72abc726c80aaa8dce0811566916b58
luoqi <903935008@qq.com>
2021-05-15 16:29:17 +0800

    feat: 增加 demo 页面，增加 PictureBox

---
+++ src/views/Demo/PictureBox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page-picture-box">
+    <div class="picture-box">
+      <img src="../../assets/logo.png" alt="" class="picture-box__img">
+      <span class="picture-box__tag">tag</span>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  /*------------------------------------*\
+    #PictureBox
+  \*------------------------------------*/
+  #PictureBox() {
+    .wrap(@width, @height) {
+      position: relative;
+      width: @width;
+      height: @height;
+    }
+    .img() {
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 1;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    .tag() {
+      position: absolute;
+      right: 8px;
+      bottom: 8px;
+      z-index: 2;
+    }
+  }
+
+  .picture-box {
+    #PictureBox.wrap(200px, 200px);
+    &__img {
+      #PictureBox.img();
+    }
+    &__tag {
+      #PictureBox.tag();
+    }
+  }
+</style>
---
+++ src/views/Demo/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-demo">
+    <ul class="demo-list">
+      <li class="demo-item">
+        <picture-box />
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+  import PictureBox from './PictureBox';
+
+
+  export default {
+    components: {
+      PictureBox,
+    },
+  };
+</script>
